be61e23690cfec5be63c44d6cf1e807bf19ee85c
Lunctas <514010@vistacollege.nl>
2023-03-03 12:57:18 +0100

    vergrootglas toegevoegd

--- zoekbalk.php
+++ zoekbalk.php
@@ -4,14 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>homepagina</title>
-    
+    <title>Document</title>
 </head>
 <link rel="stylesheet" href="home.css">
 <link rel="stylesheet" href="zoekbalk.css">
 <body>
-
-    <a href="zoekbalk.php"> <img src="WjhhVl59vCUAAAAASUVORK5CYII.png" id="vergrootglas"> </a>
+    
+<a href="zoekbalk.php"> <img src="WjhhVl59vCUAAAAASUVORK5CYII.png" id="vergrootglas"> </a>
 
 </body>
-</html> 
+</html>
