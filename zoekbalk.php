a394147df86ba4fe462b3cd6db25c057990e682a
Lunctas <514010@vistacollege.nl>
2023-03-16 13:21:13 +0100

    last test of today

--- zoekbalk.php
+++ zoekbalk.php
@@ -5,9 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="zoekbalk.css">
 </head>
-<link rel="stylesheet" href="home.css">
-<link rel="stylesheet" href="zoekbalk.css">
+
 <body>
     
 <a href="zoekbalk.php"> <img src="WjhhVl59vCUAAAAASUVORK5CYII.png" id="vergrootglas"> </a>
